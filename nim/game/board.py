0e0698581151ab2fb15c3fa7778ac877c920603b
corniab <bencornia@byui.edu>
2021-10-18 19:51:39 -0600

    created Board class with methods and doc strings

--- nim/game/board.py
+++ nim/game/board.py
@@ -1,3 +1,54 @@
 import random
 
-# TODO: Define the Board class here
+
+class Board:
+    """Designated playing surface. The responsibility of Board is to keep track of the pieces in play.
+
+    Stereotype:
+        Information Holder
+
+    Attributes:
+        _pile (integer): The pile to remove from.
+        _stones (integer): The number of stones to remove.
+    """
+
+    def __init__(self):
+        """The class constructor.
+
+        Args:
+            self (Board): An instance of Board.
+        """
+        self._prepare()
+
+    def apply(self, move):
+        """The apply method applies a move to the playing surface.
+        In this case, that means removing a number of stones from a pile.
+
+        Args:
+            self (Board): An instance of Board.
+            move: An instance of Move(class).
+        """
+
+    def is_empty(self):
+        """Determines if all the stones have been removed from the board.
+        It returns True if the board has no stones on it; false if otherwise.
+
+        Args:
+            self (Board): An instance of Board.
+        """
+
+    def to_string(self):
+        """Converts the board data to its string representation
+        and returns it to the caller.
+
+        Args:
+            self (Board): An instance of Board.
+        """
+
+    def _prepare(self):
+        """Sets up the board with a random number of piles (2 - 5)
+        containing a random number of stones (1 - 9).
+
+        Args:
+            self (Board): An instance of Board.
+        """
